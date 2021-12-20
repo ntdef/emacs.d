cc900dcff5cdc4446ca81c494823dbf0c977f110
Troy de Freitas <me@ntdef.com>
2021-12-20 10:06:04 -0600

    Add pandoc to markdown config

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -3,9 +3,12 @@
 ;;; Code:
 
 (when (maybe-require-package 'markdown-mode)
+  (setq markdown-hide-urls t)
   (add-auto-mode 'markdown-mode "\\.md\\.html\\'")
   (with-eval-after-load 'whitespace-cleanup-mode
     (add-to-list 'whitespace-cleanup-mode-ignore-modes 'markdown-mode)))
+
+(maybe-require-package 'pandoc-mode)
 
 
 (provide 'init-markdown)
